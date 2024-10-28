fdf824c0cf9458d966e4cfb4a7752e51049725a9
joongwon <sara970517@gmail.com>
2024-10-28 20:47:06 +0900

    5. 회원 관리 예제 - 웹 MVC 개발

---
+++ src/main/java/hello/hello_spring/controller/HomeController.java
@@ -0,0 +1,11 @@
+package hello.hello_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /* @Controller를 사용하면 해당 클래스를 스프링 컨테이너에 자동 등록한다.
  * 해당 클래스는 스프링 컨테이너 내부의 스프링 빈이 된다.
@@ -11,6 +17,7 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
 
     private final MemberService memberService;
+
     /* 생성자에 @Autowired가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다.
      * 이것을 DI(Dependency Injection, 의존성 주입)이라 한다,
      * DI : 객체 의존관계를 외부에서 넣어주는 것
@@ -20,6 +27,33 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // 추가: 회원 등록 폼 컨트롤러
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 추가2: 회원 실제 등록하는 기능
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        // 홈화면으로 보내기
+        return "redirect:/";
+    }
+
+    // 회원 컨트롤러에서 조회 기능
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
 
